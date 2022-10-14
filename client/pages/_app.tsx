bebeb751ed423bb65ac45efa9352ad763239cad4
YahiroRyo <honakafn@gmail.com>
2022-10-14 20:21:33 +0900

    eslintとprettier導入

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,9 @@
-import '../styles/reset.scss'
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '../styles/reset.scss';
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ format.sh
@@ -0,0 +1,2 @@
+docker compose exec blog-api ./vendor/bin/php-cs-fixer fix .
+docker compose exec blog-client yarn format
