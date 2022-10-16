478351b8d9e2a3aa24f836b9a4d1d2d0c68f393f
YahiroRyo <honakafn@gmail.com>
2022-10-17 00:19:12 +0900

    修正

--- client/components/organisms/ContainerComponents/Blog/BlogCardListContainer.tsx
+++ client/components/organisms/ContainerComponents/Blog/BlogCardListContainer.tsx
@@ -5,7 +5,15 @@ import BlogCardList from '../../PresentationalComponents/Blog/BlogCardList';
 
 const BlogCardListContainer = () => {
   const fecher = async () => {
-    return (await axios.get<Blog[]>(`${process.env.NEXT_PUBLIC_API_URL}/api/admin/blogs`)).data;
+    if (!sessionStorage.getItem('token')) return;
+
+    return (
+      await axios.get<Blog[]>(`${process.env.NEXT_PUBLIC_API_URL}/api/admin/blogs`, {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+        },
+      })
+    ).data;
   };
   const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/admin/blogs`, fecher);
 
