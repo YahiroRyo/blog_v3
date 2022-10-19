e1d957ea405c1b7a3ce6863dd3ddb6f91f1a85bb
YahiroRyo <honakafn@gmail.com>
2022-10-20 05:28:15 +0900

    送信位置変更

--- client/pages/blogs/[blogId].tsx
+++ client/pages/blogs/[blogId].tsx
@@ -40,13 +40,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     error: '',
   };
 
-  axios.post(`${process.env.NEXT_PUBLIC_SSR_API_URL}/api/blogs/${context.query.blogId}/access`, {
-    headers: JSON.stringify(context.req.headers),
-    userAgent: context.req.headers['user-agent'],
-    referer: context.req.headers.referer,
-    from: context.req.headers.from,
-  });
-
   try {
     const response = await axios.get<DetailActiveBlog>(
       `${process.env.NEXT_PUBLIC_SSR_API_URL}/api/blogs/${context.query.blogId}`,
@@ -56,6 +49,12 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     result.thumbnail = response.data.thumbnail;
     result.mainImage = response.data.mainImage;
     result.description = response.data.body.replaceAll('#', '');
+    axios.post(`${process.env.NEXT_PUBLIC_SSR_API_URL}/api/blogs/${context.query.blogId}/access`, {
+      headers: JSON.stringify(context.req.headers),
+      userAgent: context.req.headers['user-agent'],
+      referer: context.req.headers.referer,
+      from: context.req.headers.from,
+    });
   } catch (e) {
     result.error = '存在しないブログです';
   }
