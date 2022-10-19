86ba548422e1431f5a22e8db4dbca71c86f19b06
YahiroRyo <honakafn@gmail.com>
2022-10-19 10:32:15 +0900

    パス変更

--- api/packages/Domain/Blog/ValueObjects/ImageStoragePath.php
+++ api/packages/Domain/Blog/ValueObjects/ImageStoragePath.php
@@ -3,7 +3,7 @@
 namespace Packages\Domain\Blog\ValueObjects;
 
 final class ImageStoragePath {
-    private const THUMBNAIL_STORAGE_PATH  = '/images/blogs/thumb/';
+    private const THUMBNAIL_STORAGE_PATH  = '/images/blogs/thumb';
     private const MAIN_IMAGE_STORAGE_PATH = '/images/blogs';
 
     public function __construct() {
