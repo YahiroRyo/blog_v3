38ee5db4d66cc6de13e9b8d8e78b5702e414e4e7
YahiroRyo <honakafn@gmail.com>
2022-10-21 21:29:59 +0900

    更新ができないバグ修正

--- api/packages/Infrastructure/Repositories/Blog/NoSQLDetailActiveBlogAccessRepository.php
+++ api/packages/Infrastructure/Repositories/Blog/NoSQLDetailActiveBlogAccessRepository.php
@@ -3,10 +3,10 @@
 namespace Packages\Infrastructure\Repositories\Blog;
 
 use Aws\DynamoDb\DynamoDbClient;
+
 use Carbon\Carbon;
 use Packages\Infrastructure\Repositories\Blog\DetailActiveBlogAccessRepository;
 use Packages\Domain\Blog\Entities\DetailActiveBlogAccess;
-use Illuminate\Support\Str;
 
 final class NoSQLDetailActiveBlogAccessRepository implements DetailActiveBlogAccessRepository {
     private DynamoDbClient $dynamoDbClient;
@@ -16,16 +16,27 @@ final class NoSQLDetailActiveBlogAccessRepository implements DetailActiveBlogAcc
     }
 
     public function access(DetailActiveBlogAccess $detailActiveBlogAccess): void {
+        $response = $this->dynamoDbClient->updateItem([
+            'TableName' => 'blogAccessesSequence',
+            'Key'       => [
+                'name'  => ['S' => 'sequence'],
+            ],
+            'UpdateExpression' => 'SET #currentNumber = #currentNumber + :val',
+            'ExpressionAttributeNames'  => ['#currentNumber' => 'currentNumber'],
+            'ExpressionAttributeValues' => [':val' => ['N' => '1']],
+            'ReturnValues' => 'UPDATED_NEW'
+        ]);
+
         $this->dynamoDbClient->putItem([
             'TableName' => 'blogAccesses',
             'Item'      => [
-                'id'                => ['S' => (string) Str::uuid()],
+                'id'                => ['N' => $response['Attributes']['currentNumber']['N']],
                 'blogId'            => ['S' => $detailActiveBlogAccess->blogId()->value()],
                 'headers'           => ['S' => $detailActiveBlogAccess->headers()->value()],
                 'userAgent'         => ['S' => $detailActiveBlogAccess->userAgent()->value()],
                 'referer'           => ['S' => $detailActiveBlogAccess->referer()->value()],
                 'from'              => ['S' => $detailActiveBlogAccess->from()->value()],
-                'accessedAt'        => ['S' => Carbon::now()->toDateTimeString()],
+                'accessedAt'        => ['S' => Carbon::now()->toDateString()],
             ]
         ]);
     }
