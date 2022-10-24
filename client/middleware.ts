7d1c3ed1630e4ea4ae66ac1328ad1bf7fb2656e0
YahiroRyo <honakafn@gmail.com>
2022-10-24 21:32:11 +0900

    middlewareでアクセス数の制限を設けた

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,23 +1,31 @@
-import axios from 'axios';
-import fetchAdapter from '@vespaiach/axios-fetch-adapter';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const middleware = async (req: NextRequest) => {
-  if (req.nextUrl.pathname.search(/^\/blogs\/[a-zA-Z0-9]{26}/) === 0 && req.nextUrl.searchParams.get('blogId')) {
-    await fetch(`${process.env.NEXT_PUBLIC_SSR_API_URL}/api/blogs/${req.nextUrl.searchParams.get('blogId')}/access`, {
-      method: 'POST',
-      body: JSON.stringify({
-        headers: JSON.stringify(req.headers) ?? '',
-        userAgent: req.headers.get('user-agent') ?? '',
-        referer: req.headers.get('referer') ?? '',
-        from: req.headers.get('from') ?? '',
-      }),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
+  if (req.nextUrl.pathname.search(/^\/blogs\/[a-zA-Z0-9]{26}/) !== 0 || !req.nextUrl.searchParams.get('blogId')) {
+    return NextResponse.next(req);
   }
+
+  if (req.cookies.has(`blogs/accessed/${req.nextUrl.searchParams.get('blogId')}`)) {
+    return NextResponse.next(req);
+  }
+
+  await fetch(`${process.env.NEXT_PUBLIC_SSR_API_URL}/api/blogs/${req.nextUrl.searchParams.get('blogId')}/access`, {
+    method: 'POST',
+    body: JSON.stringify({
+      headers: JSON.stringify(req.headers) ?? '',
+      userAgent: req.headers.get('user-agent') ?? '',
+      referer: req.headers.get('referer') ?? '',
+      from: req.headers.get('from') ?? '',
+    }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  req.cookies.set(`blogs/accessed/${req.nextUrl.searchParams.get('blogId')}`, true, {
+    maxAge: 1800,
+  });
 
   return NextResponse.next(req);
 };
