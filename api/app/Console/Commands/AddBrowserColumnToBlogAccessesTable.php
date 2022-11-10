ca227d8953bfa1d0279ab5c7d176c3523ec57741
YahiroRyo <honakafn@gmail.com>
2022-11-10 21:02:13 +0900

    ブラウザの情報を付加

---
+++ api/app/Console/Commands/AddBrowserColumnToBlogAccessesTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Aws\DynamoDb\DynamoDbClient;
+use Illuminate\Console\Command;
+use Packages\Domain\Blog\ValueObjects\UserAgent;
+
+class AddBrowserColumnToBlogAccessesTable extends Command {
+    private DynamoDbClient $dynamoDbClient;
+    protected $signature   = 'addColumn:blogAccesses:browser';
+    protected $description = 'Command description';
+
+    public function __construct(DynamoDbClient $dynamoDbClient) {
+        $this->dynamoDbClient = $dynamoDbClient;
+    }
+
+    public function handle(): int {
+        $response = $this->dynamoDbClient->getItem([
+            'TableName' => 'blogAccessesSequence',
+            'Key'       => [
+                'name'  => ['S' => 'sequence'],
+            ]
+        ]);
+
+        $response = $this->dynamoDbClient->scan([
+            'TableName'                 => 'blogAccesses',
+            'FilterExpression'          => '#id BETWEEN :fromId AND :toId',
+            'ExpressionAttributeNames'  => [
+                '#id'       => 'id',
+                '#blogId'   => 'blogId'
+            ],
+            'ExpressionAttributeValues' => [
+                ':fromId'               => ['N' => '1'],
+                ':toId'                 => ['N' => $response['Item']['currentNumber']['N']],
+            ]
+        ]);
+
+        foreach ($response['Items'] as $item) {
+            $this->dynamoDbClient->updateItem([
+                'TableName' => 'blogAccesses',
+                'Key'       => [
+                    'id'  => ['N' => $item['id']['N']],
+                ],
+                'UpdateExpression'          => 'SET #browser = :val',
+                'ExpressionAttributeNames'  => ['#browser' => 'browser'],
+                'ExpressionAttributeValues' => [':val' => ['S' => UserAgent::of($item['userAgent']['S'])->browser()->value]],
+            ]);
+        }
+
+        return Command::SUCCESS;
+    }
+}
--- api/packages/Domain/Blog/ValueObjects/UserAgent.php
+++ api/packages/Domain/Blog/ValueObjects/UserAgent.php
@@ -8,6 +8,29 @@ use Packages\Domain\StringLengthLimit;
 final class UserAgent extends StringLengthLimit {
     use NoSQLColumnString;
 
-    protected int $lengthLimit = 2000;
-    protected string $name     = 'ユーザー情報';
+    protected int $lengthLimit      = 2000;
+    protected string $name          = 'ユーザー情報';
+    private array $regexBrowsers    = [
+        '/Firefox/i'    => Browser::FIREFOX,
+        '/OPR/i'        => Browser::OPERA,
+        '/Chrome/i'     => Browser::CHROME,
+        '/Safari/i'     => Browser::SAFARI,
+        '/Netscape/i'   => Browser::NETSCAPE,
+        '/Edge/i'       => Browser::EDGE,
+        '/Trident/i'    => Browser::INTERNET_EXPLORER,
+    ];
+
+    public function browser(): Browser {
+        foreach ($this->regexBrowsers as $regex => $browser) {
+            if (preg_match($regex, $this->value)) {
+                return $browser;
+            }
+        }
+
+        return Browser::UNKNOWN;
+    }
+
+    public static function of($value): UserAgent {
+        return parent::of($value);
+    }
 }
